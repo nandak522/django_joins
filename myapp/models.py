cd64f20c31d2c4c87c1fda1821554f651849ce89
Nanda Kishore <madhav.bnk@gmail.com>
2011-05-12 08:11:25 +0530

    Adding Models

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,43 @@
-from django.db import models
+"""
+Question Title
+Question Description
+Question Comments count
+All Answers descriptions
+All Answer comments
+Answer author names
+Assuming I have a hover action on the Author Link, which displays information like: Followers count, Mentions count, Reputation Score
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Question(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=500, blank=True, null=True)
+    raised_by = models.ForeignKey(User)
+    
+class Answer(models.Model):
+    description = models.CharField(max_length=500, blank=True, null=True)
+    given_by = models.ForeignKey(User)
+    question = models.ForeignKey(Question)
+
+class QuestionComment(models.Model):
+    description = models.CharField(max_length=200)
+    author = models.ForeignKey(User)
+    question = models.ForeignKey(Question)
+    
+class AnswerComment(models.Model):
+    description = models.CharField(max_length=200)
+    author = models.ForeignKey(User)
+    question = models.ForeignKey(Answer)
+    
+class Reputation(models.Model):
+    user = models.ForeignKey(User)
+    points = models.IntegerField(default=0)
+
+class Follower(models.Model):
+    user = models.ForeignKey(User)
+    following = models.ForeignKey(User)
+    
+class Mention(models.Model):
+    user = models.ForeignKey(User)
